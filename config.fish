722837b85803c999f4bb0b3f59143d44ad93a2d7
parksb <parkgds@gmail.com>
2023-09-02 00:52:39 +0900

    config.fish를 유연하게 개선

--- config.fish
+++ config.fish
@@ -1,11 +1,23 @@
 # local variables
-set -l OS (uname)
+set -l os (uname)
+
+# local functions
+function alias_if_exists
+  if type -q $argv[1]
+    alias $argv[2]=$argv[3]
+  end
+end
+
+function fish_add_path_if_exists
+  if test -e $argv[1]
+    fish_add_path $argv[1]
+  end
+end
 
 # editors
-alias ij='idea'
-alias v='nvim'
-alias vi='nvim'
-alias vim='nvim'
+alias_if_exists nvim 'v' 'nvim'
+alias_if_exists nvim 'vi' 'nvim'
+alias_if_exists nvim 'vim' 'nvim'
 
 # configs
 alias zshconf='v ~/.zshrc'
@@ -17,105 +29,123 @@ alias alacrittyconf='v ~/.alacritty.yml'
 alias fishconf='v ~/.config/fish/config.fish'
 
 # coreutils alternatives
-alias ls='lsd'
-alias tree='lsd --tree'
-alias cat='bat'
-alias find='fd'
-alias ps='procs'
-alias grep='rg'
-alias diff='delta'
+alias_if_exists lsd 'ls' 'lsd'
+alias_if_exists lsd 'tree' 'lsd --tree'
+alias_if_exists bat 'cat' 'bat'
+alias_if_exists fd 'find' 'fd'
+alias_if_exists procs 'ps' 'procs'
+alias_if_exists rg 'grep' 'rg'
+alias_if_exists delta 'diff' 'delta'
 
 # node
-fish_add_path ~/.nvm/versions/node/(node -v)/bin
+if type -q nvm && type -q node
+  fish_add_path ~/.nvm/versions/node/(node -v)/bin
+end
 
 # npm
-alias ni='npm install'
-alias ns='npm start'
-alias nt='npm test'
-alias nr='npm run'
-alias nls='npm list --depth 0'
+if type -q npm
+  alias ni='npm install'
+  alias ns='npm start'
+  alias nt='npm test'
+  alias nr='npm run'
+  alias nls='npm list --depth 0'
+end
 
 # pnpm
-alias pni='pnpm install'
-alias pns='pnpm start'
-alias pnt='pnpm test'
-if test "$OS" = Darwin
-  set -gx PNPM_HOME "~/Library/pnpm"
-  fish_add_path $PNPM_HOME
+if type -q pnpm
+  alias pni='pnpm install'
+  alias pns='pnpm start'
+  alias pnt='pnpm test'
+
+  if test $os = Darwin
+    set -gx PNPM_HOME "~/Library/pnpm"
+    fish_add_path $PNPM_HOME
+  end
 end
 
 # gradle
 alias gd='./gradlew'
 
 # cargo
-alias cb='cargo build'
-alias cr='cargo run'
-alias ck='cargo check'
-alias cl='cargo fmt && cargo clippy'
-alias rs='cargo script'
+if type -q cargo
+  alias cb='cargo build'
+  alias cr='cargo run'
+  alias ck='cargo check'
+  alias cl='cargo fmt && cargo clippy'
+  alias rs='cargo script'
+end
 
 # python
-alias py='python'
-set -gx PYENV_ROOT ~/.pyenv
-fish_add_path $PYENV_ROOT/bin
-pyenv init - | source
+if type -q pyenv
+  set -gx PYENV_ROOT ~/.pyenv
+  fish_add_path_if_exists $PYENV_ROOT/bin
+  pyenv init - | source
+end
+alias_if_exists python 'py' 'python'
 
 # docker
-alias dk='docker'
-alias dkp='docker ps'
-alias dkk='docker stop (docker ps -q)'
-alias dki='docker inspect --format="{{.Id}}"'
-alias dks='docker start'
+if type -q docker
+  alias dk='docker'
+  alias dkp='docker ps'
+  alias dkk='docker stop (docker ps -q)'
+  alias dki='docker inspect --format="{{.Id}}"'
+  alias dks='docker start'
+end
 
 # tmux
-alias tx='tmux'
-alias txn='tmux new -s'
-alias txl='tmux choose-tree'
-alias txk='tmux kill-session -t'
-alias txka='tmux kill-session -a'
-fish_add_path ~/.tmux/plugins/tpm
+if type -q tmux
+  alias tx='tmux'
+  alias txn='tmux new -s'
+  alias txl='tmux choose-tree'
+  alias txk='tmux kill-session -t'
+  alias txka='tmux kill-session -a'
+  fish_add_path_if_exists ~/.tmux/plugins/tpm
+end
 
 # fzf
-alias fh='history | fzf'
+if type -q fzf
+  alias fh='history | fzf'
+end
 
 # tectonic
-alias texn='tex new pure'
-alias texb='tex build'
-alias texw='tex watch'
-alias texo='tex open'
+if type -q tex
+  alias texn='tex new pure'
+  alias texb='tex build'
+  alias texw='tex watch'
+  alias texo='tex open'
+end
 
 # others
-alias rm='trash'
-alias mv='mv -i'
 alias clr='clear'
-alias g='git'
-alias ts='ts-node'
 alias please='sudo'
-alias az5='docker stop (docker ps -q) and pm2 kill and tmux kill-session -a and tmux ls'
+alias mv='mv -i'
 alias monitor='top -o vsize'
-alias sql='mycli'
-alias j='z'
-alias img='imgcat'
-alias marp='npx @marp-team/marp-cli@latest'
+alias_if_exists git 'g' 'git'
+alias_if_exists trash 'rm' 'trash'
+alias_if_exists mycli 'sql' 'mycli'
+alias_if_exists z 'j' 'z'
+alias_if_exists imgcat 'img' 'imgcat'
+alias_if_exists npx 'ts' 'npx tsx'
+alias_if_exists npx 'marp' 'npx @marp-team/marp-cli@latest'
 
 # universal fish user paths for binaries
-if test "$OS" = Darwin
-  fish_add_path /opt/homebrew/bin
-  fish_add_path /opt/homebrew/opt/libpq/bin
+if test $os = Darwin
+  fish_add_path_if_exists /opt/homebrew/bin
+  fish_add_path_if_exists /opt/homebrew/opt/libpq/bin
 end
-fish_add_path /opt/local/bin
-fish_add_path ~/.deno/bin
-fish_add_path ~/.cargo/bin
-fish_add_path ~/.cabal/bin
-fish_add_path ~/.ghcup/bin
-fish_add_path ~/bin
-# to erase one: `$ fish_user_path erase`
+fish_add_path_if_exists /opt/local/bin
+fish_add_path_if_exists ~/.deno/bin
+fish_add_path_if_exists ~/.cargo/bin
+fish_add_path_if_exists ~/.cabal/bin
+fish_add_path_if_exists ~/.ghcup/bin
+fish_add_path_if_exists ~/bin
+# to erase one: `fish_user_path erase`
 
 # env variables
-set -gx BAT_THEME Dracula
-set -gx EDITOR nvim
-set -gx PROJECT_DIR ~/projects
-set -gx TEX_PREAMBLES_DIR ~/Documents/TeX/preambles
+if type -q bat; set -gx BAT_THEME Dracula; end
+if type -q nvim; set -gx EDITOR nvim; end
+if test -e ~/projects; set -gx PROJECT_DIR ~/projects; end
+if test -e ~/projects; set -gx TEX_PREAMBLES_DIR ~/Documents/TeX/preambles; end
 
 # theme
 fish_config theme choose "Dracula Official"
