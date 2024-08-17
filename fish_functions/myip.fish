7b6a37c5601c00bf62da527e975b9ceb854e3dd6
parksb <parkgds@gmail.com>
2024-08-17 17:00:57 +0900

    git stash, myip, fisher list 수정

--- fish_functions/myip.fish
+++ fish_functions/myip.fish
@@ -1,4 +1,4 @@
 function myip
   echo "Local: $(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')"
-  echo "Public: $(curl -s ifconfig.me)"
+  echo "Public: $(curl -s https://checkip.amazonaws.com/)"
 end
