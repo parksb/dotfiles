05307284ca1f41020ab5380863ac8f4197b0b062
parksb <parkgds@gmail.com>
2025-04-12 12:40:03 +0900

    git-conflict prev/next 단축키 변경

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -14,7 +14,7 @@ return {
     vim.keymap.set("n", ",t", "<Plug>(git-conflict-theirs)", { desc = "git-conflict-theirs" })
     vim.keymap.set("n", ",b", "<Plug>(git-conflict-both)", { desc = "git-conflict-both" })
     vim.keymap.set("n", ",0", "<Plug>(git-conflict-none)", { desc = "git-conflict-none" })
-    vim.keymap.set("n", "[x", "<Plug>(git-conflict-prev-conflict)", { desc = "git-conflict-prev-conflict" })
-    vim.keymap.set("n", "]x", "<Plug>(git-conflict-next-conflict)", { desc = "git-conflict-next-conflict" })
+    vim.keymap.set("n", ",x", "<Plug>(git-conflict-prev-conflict)", { desc = "git-conflict-prev-conflict" })
+    vim.keymap.set("n", ",X", "<Plug>(git-conflict-next-conflict)", { desc = "git-conflict-next-conflict" })
   end,
 }
