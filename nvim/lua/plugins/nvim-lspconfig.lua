8ede36bcfb30cdde4a9a11bf2643bc877df03a8b
parksb <parkgds@gmail.com>
2025-05-10 16:16:11 +0900

    lspconfig 및 mason 업데이트

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,8 +2,8 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "saghen/blink.cmp",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
     "nvim-telescope/telescope.nvim",
   },
   init = function()
@@ -35,10 +35,11 @@ return {
     },
   },
   config = function()
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-    local lspconfig = require("lspconfig")
+    local function opts(desc)
+      return { noremap = true, silent = true, desc = desc }
+    end
 
-    local ensure_installed = {
+    local servers = {
       lua_ls = {
         settings = require("plugins/langs/lua").lspconfig,
       },
@@ -50,11 +51,23 @@ return {
       dockerls = {},
       html = {},
       ts_ls = {
-        root_dir = lspconfig.util.root_pattern("package.json"),
-        single_file_support = false,
+        root_dir = function(_, callback)
+          local deno_dir = vim.fs.root(0, { "deno.json", "deno.jsonc" })
+          local root_dir = vim.fs.root(0, { "tsconfig.json", "jsconfig.json", "package.json" })
+          if root_dir and deno_dir == nil then
+            callback(root_dir)
+          end
+        end,
+        root_markers = { "tsconfig.json", "jsconfig.json", "package.json" },
       },
       denols = {
-        root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+        root_dir = function(_, callback)
+          local root_dir = vim.fs.root(0, { "deno.json", "deno.jsonc" })
+          if root_dir then
+            callback(root_dir)
+          end
+        end,
+        root_markers = { "deno.json", "deno.jsonc" },
       },
       jsonls = {},
       marksman = {},
@@ -62,44 +75,31 @@ return {
       yamlls = {},
     }
 
+    for server, config in pairs(servers) do
+      vim.lsp.config(server, config)
+    end
+
+    vim.keymap.set(
+      "n",
+      "gd",
+      "<CMD>Telescope lsp_definitions reuse_win=true theme=dropdown<CR>",
+      opts("Go to definition")
+    )
+    vim.keymap.set(
+      "n",
+      "gi",
+      "<CMD>Telescope lsp_implementations reuse_win=true theme=dropdown<CR>",
+      opts("Go to implementation")
+    )
+    vim.keymap.set("n", "gr", "<CMD>Telescope lsp_references theme=dropdown<CR>", opts("Go to references"))
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Show documentation"))
+    vim.keymap.set("n", "X", vim.diagnostic.open_float, opts("Show diagnostics"))
+    vim.keymap.set("n", "<LEADER>rn", vim.lsp.buf.rename, opts("Rename symbol"))
+    vim.keymap.set("n", "<LEADER>ca", vim.lsp.buf.code_action, opts("Show code actions"))
+
     require("mason").setup()
     require("mason-lspconfig").setup({
-      ensure_installed = vim.tbl_keys(ensure_installed),
-    })
-
-    require("mason-lspconfig").setup_handlers({
-      function(server)
-        lspconfig[server].setup({
-          capabilities = vim.deepcopy(capabilities),
-
-          settings = ensure_installed[server].settings,
-          root_dir = ensure_installed[server].root_dir,
-          single_file_support = ensure_installed[server].single_file_support,
-
-          on_attach = function(_, buf)
-            local opts = function(desc)
-              return { noremap = true, silent = true, buffer = buf, desc = desc }
-            end
-            vim.keymap.set(
-              "n",
-              "gd",
-              "<CMD>Telescope lsp_definitions reuse_win=true theme=dropdown<CR>",
-              opts("Go to definition")
-            )
-            vim.keymap.set(
-              "n",
-              "gi",
-              "<CMD>Telescope lsp_implementations reuse_win=true theme=dropdown<CR>",
-              opts("Go to implementation")
-            )
-            vim.keymap.set("n", "gr", "<CMD>Telescope lsp_references theme=dropdown<CR>", opts("Go to references"))
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Show documentation"))
-            vim.keymap.set("n", "X", vim.diagnostic.open_float, opts("Show diagnostics"))
-            vim.keymap.set("n", "<LEADER>rn", vim.lsp.buf.rename, opts("Rename symbol"))
-            vim.keymap.set("n", "<LEADER>ca", vim.lsp.buf.code_action, opts("Show code actions"))
-          end,
-        })
-      end,
+      ensure_installed = vim.tbl_keys(servers),
     })
   end,
 }
