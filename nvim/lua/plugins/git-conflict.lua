889e0679ae6da831137327f026ecba19af69e1ee
parksb <parkgds@gmail.com>
2025-05-16 09:54:05 +0900

    git-conflict 키맵 변경

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -7,14 +7,14 @@ return {
     vim.api.nvim_set_hl(0, "DiffAdd", { fg = "#ffffff", bg = "#1d3450" })
   end,
   opts = {
-    default_mappings = false,
+    default_mappings = {
+      ours = ",o",
+      theirs = ",t",
+      none = ",0",
+      both = ",2",
+      next = ",x",
+      prev = ",X",
+    },
   },
-  config = function()
-    vim.keymap.set("n", ",o", "<Plug>(git-conflict-ours)", { desc = "git-conflict-ours" })
-    vim.keymap.set("n", ",t", "<Plug>(git-conflict-theirs)", { desc = "git-conflict-theirs" })
-    vim.keymap.set("n", ",b", "<Plug>(git-conflict-both)", { desc = "git-conflict-both" })
-    vim.keymap.set("n", ",0", "<Plug>(git-conflict-none)", { desc = "git-conflict-none" })
-    vim.keymap.set("n", ",x", "<Plug>(git-conflict-prev-conflict)", { desc = "git-conflict-prev-conflict" })
-    vim.keymap.set("n", ",X", "<Plug>(git-conflict-next-conflict)", { desc = "git-conflict-next-conflict" })
-  end,
+  config = true,
 }
