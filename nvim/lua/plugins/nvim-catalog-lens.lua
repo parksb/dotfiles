9efab0e0791a326ac1fe54641dc52990b3444691
parksb <parkgds@gmail.com>
2025-10-18 23:08:28 +0900

    nvim-catalog-lens 설정 업데이트

--- nvim/lua/plugins/nvim-catalog-lens.lua
+++ nvim/lua/plugins/nvim-catalog-lens.lua
@@ -2,7 +2,7 @@ return {
   "parksb/nvim-catalog-lens",
   branch = "main",
   ft = { "json" },
-  init = function()
-    vim.g.catalog_display = "eol"
-  end,
+  opts = {
+    display = "eol"
+  },
 }
