5240a7c28ede51ddc76b92129275d100ebdc9252
parksb <parkgds@gmail.com>
2025-11-30 18:53:46 +0900

    ts-error-translator.nvim 2.0 대응

--- nvim/lua/plugins/ts-error-translator.lua
+++ nvim/lua/plugins/ts-error-translator.lua
@@ -1,9 +1,6 @@
 return {
   "dmmulroy/ts-error-translator.nvim",
-  config = function()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx)
-      require("ts-error-translator").translate_diagnostics(err, result, ctx)
-      vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
-    end
-  end,
+  opts = {
+    auto_attach = true,
+  },
 }
